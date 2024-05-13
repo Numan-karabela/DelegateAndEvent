a4d18ca40029310f2c69a46126fdb8895be6c0be
1453n <1453n@DESKTOP-PHGFDVP>
2024-05-13 18:23:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿string path = @"";
+PathControl pathControl = new();
+
+
+pathControl.pathHnadlerevent += (SizeMb) =>
+{
+    Console.WriteLine("Boyut 50 Mb geçmiştir ");
+};
+await pathControl.Controll(path);
+
+
+class PathControl
+{
+    public delegate void PathHandler(long SizeMb);
+    public event PathHandler pathHnadlerevent;
+
+    public async Task Controll(string path)
+    {
+        while (true)
+        {
+
+            await Task.Delay(1500);
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            var file = directoryInfo.GetFiles();
+            var size = await Task.Run(() => directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length));
+            var sizeMb = (size / 1024) / 1024;
+            if (sizeMb > 50)
+            {
+                pathHnadlerevent(sizeMb);
+            }
+
+        }
+
+
+
+    }
+
+
+}
